78445a6a602e71d00f094706716365e02da33622
Rodrigo Contreras <rodrigo.cp993@gmail.com>
2025-01-15 15:10:09 -0500

    modulo 1

---
+++ Dia_4/3-crud_alumnos.py
@@ -0,0 +1,6 @@
+from matriculas.lib_alumnos import *
+
+registro()
+
+matricula()
+
---
+++ Dia_4/matriculas/lib_alumnos.py
@@ -0,0 +1,8 @@
+def registro():
+    print(f'regitrando a un nuevo alumno')
+
+def matricula():
+    print(f'matricula de alumno')
+
+
+    
